c5a80fa9951f557a8caced1cda3d43f4308a2cca
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-06-09 15:36:16 +0530

    form layout

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,8 +4,8 @@ function HeroSection(){
         <div className="rounded-b-full justify-center items-center flex flex-col bg-linear-to-b from-red-950 to-red-700 h-[60vh] px-20">
             <h1 className="text-[50px] font-bold text-white">BOOK YOUR CAR RENTAL</h1>
             <h1 className="text-2xl text-white">Find rental cars, SUVs and premium vehicles at over 10,000 locations worldwide.</h1>
-            <form className="bg-white h-[20vh] w-[70vw] mt-5 rounded">
-            
+            <form className="bg-white h-[25vh] w-[70vw] mt-5 rounded">
+                
             </form>
         </div>
     )
